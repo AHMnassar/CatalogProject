e660651d784839a96056849511a962a5c1618a30
AHMnassar <42746567+AHMnassar@users.noreply.github.com>
2019-01-25 14:14:01 +0200

    Update MoviesLists.py

--- MoviesLists.py
+++ MoviesLists.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from DB_setup import Year, Base, CatalogItem, User
 
-engine = create_engine('postgresql://catalog:password@localhost/catalog')
+engine = create_engine('postgresql://catalog:cataloggrader@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
